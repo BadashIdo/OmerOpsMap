a6bc6efe7204ca73197f9993ccf7abc8ffefc6c6
Sahar Eitam <saharei@post.bgu.ac.il>
2025-12-17 12:39:58 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,13 +1,11 @@
 import Home from './pages/Home';
 import Admin from './pages/Admin';
-import Parks from './pages/Parks';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "Home": Home,
     "Admin": Admin,
-    "Parks": Parks,
 }
 
 export const pagesConfig = {
